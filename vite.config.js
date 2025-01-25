2e9eecbd1837caf03442d49c7fc2f3436446838f
yusuf <yusufabeed@outlook.com>
2025-01-25 19:48:01 +0000

    goonudxv

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+), ...)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MyWebsite",
+  base: "/Website/",
   
 })
