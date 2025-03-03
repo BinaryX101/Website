8ab27a93c0dfe27ccd8628854a1e385a3f267b53
yusuf <yusufabeed@outlook.com>
2025-03-03 21:57:51 +0000

    gr

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
