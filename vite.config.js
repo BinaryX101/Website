6669e73eaf72db5762e3bf712161051a013d7f49
yusuf <yusufabeed@outlook.com>
2025-03-03 21:38:11 +0000

    try 67

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Website/', // Ensure this matches your GitHub repo name
+  base: '/Website/', 
 });
